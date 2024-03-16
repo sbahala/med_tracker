67fd3c0795eec6654da4477f28606567591b32fd
yuzhi-wang <yuzhi.wang.0@gmail.com>
2024-03-16 07:09:50 -0400

    Revert "Update adminDashboard.jsx"
    This reverts commit 2eb173727c942979290b500c841ad0eb97a0a5ca.

--- med-tracker/src/Components/admin/adminDashboard.jsx
+++ med-tracker/src/Components/admin/adminDashboard.jsx
@@ -4,7 +4,7 @@ import { signOut } from 'firebase/auth';
 import { auth } from '../../firebase';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
-import { addAppointment, getAppointments } from "../service/appointmentService";
+import {addAppointment, getAppointments} from "../service/appointmentService";
 import AppointmentForm from "../service/appointmentForm";
 
 const AdminDashboard = () => {
@@ -13,27 +13,23 @@ const AdminDashboard = () => {
     const [appointments, setAppointments] = useState([]);
 
     useEffect(() => {
-        const fetchAppointments = async () => {
-            try {
-                const loadedAppointments = await getAppointments();
-                setAppointments(loadedAppointments.map(appointment => ({
-                    title: `${appointment.departmentName} - ${appointment.doctorName}`,
-                    start: `${appointment.date}T${appointment.time}`,
-                    
-                })));
-            } catch (error) {
-                console.error('Failed to fetch appointments:', error);
-            }
-        };
-
         (async () => {
             try {
                 await fetchAppointments();
             } catch (error) {
-                console.error('Failed to fetch appointments:', error);
+                console.error('获取预约数据失败:', error);
             }
         })();
     }, []);
+
+    const fetchAppointments = async () => {
+        const loadedAppointments = await getAppointments();
+        setAppointments(loadedAppointments.map(appointment => ({
+            title: `${appointment.departmentName} - ${appointment.doctorName}`,
+            start: `${appointment.date}T${appointment.time}`,
+            // 可以添加更多的属性，以适应FullCalendar的事件格式
+        })));
+    };
 
     const handleSaveAppointment = async (appointmentData) => {
         console.log('Saving new appointment:', appointmentData);
@@ -41,16 +37,18 @@ const AdminDashboard = () => {
             await addAppointment(appointmentData);
             await fetchAppointments();
         } catch (error) {
-            console.error('Failed to save appointment:', error);
+            console.error("Failed to save appointment:", error);
+
         }
     };
+
 
     const handleSignOut = async () => {
         try {
             await signOut(auth);
             navigate('/');
         } catch (error) {
-            console.error('Error signing out:', error);
+            console.error("Error signing out: ", error);
         }
     };
 
