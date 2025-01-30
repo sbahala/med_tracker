6f6706a9c981439ce1bfdcb968e1de3f30d99550
sbahala <112838583+sbahala@users.noreply.github.com>
2025-01-30 14:55:27 -0500

    firebase connection

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,23 @@ import { getAuth } from "firebase/auth"
 import { getStorage } from "firebase/storage"
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
+/*const firebaseConfig = {
     apiKey: "AIzaSyArL8xR4WsE1KroHleIqU4OdFpCo0d1Z5A",
     authDomain: "med-tracker-13568.firebaseapp.com",
     projectId: "med-tracker-13568",
     storageBucket: "med-tracker-13568.appspot.com",
     messagingSenderId: "497399430338",
     appId: "1:497399430338:web:e9e0255948fd76dbc88f5b"
-};
+};*/
+//new details
+const firebaseConfig = {
+    apiKey: "AIzaSyDTKksuUeE8S6tkg_vhgiyaMYickmkUbLs",
+    authDomain: "medtracknew.firebaseapp.com",
+    projectId: "medtracknew",
+    storageBucket: "medtracknew.firebasestorage.app",
+    messagingSenderId: "3613783426",
+    appId: "1:3613783426:web:08ae90046ffb2274440b9b"
+  };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
